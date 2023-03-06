b9ff994a9f0c41fae7e59949d86e669b829d4027
jeronimo-a <jeronimoaa@al.insper.edu.br>
2023-03-05 23:30:57 -0300

    LAB 03 conceito B feito

--- 3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
+++ 3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
@@ -11,6 +11,11 @@
 #define LED_IDX      8
 #define LED_IDX_MASK (1 << LED_IDX)
 
+#define BUZZ_PIO      PIOC
+#define BUZZ_PIO_ID   ID_PIOC
+#define BUZZ_IDX      19
+#define BUZZ_IDX_MASK (1 << BUZZ_IDX)
+
 #define OLED_SW1_PIO				PIOD						// periferico que controla o SW 1 no modulo OLED
 #define OLED_SW1_PIO_ID				ID_PIOD						// ID do periferico PIOD
 #define OLED_SW1_IDX				28							// ID do pino conectado ao SW 1 do modulo OLED
@@ -27,33 +32,47 @@
 #define OLED_SW3_IDX_MASK			(1 << OLED_SW3_IDX)			// mascara para controlarmos o SW 3 do modulo OLED
 
 // flags dos botoes
+volatile char DIMINUIR = 0;
 volatile char SW1 = 0;
 volatile char SW2 = 0;
 volatile char SW3 = 0;
 
 // globais
+char reduziu = 0;
+int contagem_SW1 = 0;
+int contagem_reducao_SW1;
 const char end_str[] = " Hz  ";
 const int variacao_por_clique = 1;
-const int frequencia_maxima = 50;
+const int frequencia_maxima = 1000;
 const int frequencia_minima = 1;
 int meio_periodo_ms;
 int frequencia = 5;
+char piscando = 1;
 
+void update_oled() {
+	char frequencia_str[5];
+	sprintf(frequencia_str, "%d", frequencia);
+	strcat(frequencia_str, end_str);
+	gfx_mono_draw_string(frequencia_str, 50,16, &sysfont);
+}
 
 void callback_sw1() {
 	if (pio_get(OLED_SW1_PIO, PIO_INPUT, OLED_SW1_IDX_MASK)) {
 		SW1 = 0;
+		if (contagem_SW1 < contagem_reducao_SW1) {
+			if (frequencia <= frequencia_maxima - variacao_por_clique) {
+				frequencia += variacao_por_clique;
+			}
+		}
 	} else {
 		SW1 = 1;
-		if (frequencia >= frequencia_minima + variacao_por_clique) {
-			frequencia -= variacao_por_clique;
-		}
 	}
 }
 
 void callback_sw2() {
 	if (pio_get(OLED_SW2_PIO, PIO_INPUT, OLED_SW2_IDX_MASK)) {
 		SW2 = 0;
+		piscando = !piscando;
 	} else {
 		SW2 = 1;
 	}
@@ -64,28 +83,14 @@ void callback_sw3() {
 		SW3 = 0;
 	} else {
 		SW3 = 1;
-		if (frequencia <= frequencia_maxima - variacao_por_clique) {
-			frequencia += variacao_por_clique;
+		if (frequencia >= frequencia_minima + variacao_por_clique) {
+			frequencia -= variacao_por_clique;
 		}
 	}
 }
 
-// pisca led N vez no periodo T
-void pisca_led(int n, int t){
-	for (int i=0;i<n;i++){
-		pio_clear(LED_PIO, LED_IDX_MASK);
-		delay_ms(t);
-		pio_set(LED_PIO, LED_IDX_MASK);
-		delay_ms(t);
-	}
-}
 
-
-void init() {
-	
-	board_init();
-	sysclk_init();
-	delay_init();
+void pio_init() {
 	
 	// habilita os PIOs que usaremos
 	pmc_enable_periph_clk(ID_PIOA);
@@ -98,7 +103,7 @@ void init() {
 	pio_set_input(PIOD, OLED_SW1_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
 	
 	// define o LED como output
-	pio_configure(PIOC, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	pio_configure(PIOC, PIO_OUTPUT_0, LED_IDX_MASK | BUZZ_IDX_MASK, PIO_DEFAULT);
 	
 	// define interrupcoes e associa a uma funcao de callback
 	pio_handler_set(PIOA, ID_PIOA, OLED_SW3_IDX_MASK, PIO_IT_EDGE, callback_sw3);
@@ -115,37 +120,69 @@ void init() {
 	
 	// habilita interrupcoes nos PIOS dos botoes com prioridade 4
 	NVIC_EnableIRQ(ID_PIOA);
- 	NVIC_EnableIRQ(ID_PIOC);
- 	NVIC_EnableIRQ(ID_PIOD);
+	NVIC_EnableIRQ(ID_PIOC);
+	NVIC_EnableIRQ(ID_PIOD);
 	NVIC_SetPriority(ID_PIOA, 4);
- 	NVIC_SetPriority(ID_PIOC, 4);
- 	NVIC_SetPriority(ID_PIOD, 4);
+	NVIC_SetPriority(ID_PIOC, 4);
+	NVIC_SetPriority(ID_PIOD, 4);
+	
+}
+
+
+void init() {
+	
+	board_init();
+	sysclk_init();
+	delay_init();
+	
+	pio_init();
 
 	// Init OLED
 	gfx_mono_ssd1306_init();
 	
 	// desenhos estaticos no OLED
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-	gfx_mono_draw_string("teste", 50,16, &sysfont);
+	update_oled();
 }
 
 
 
 int main (void) {
+	
 	init();
   
 	while(1) {
 		
-		char frequencia_str[5];
-		meio_periodo_ms = 1000 / (2 * frequencia);
-		sprintf(frequencia_str, "%d", frequencia);
-		strcat(frequencia_str, end_str);
-		gfx_mono_draw_string(frequencia_str, 50,16, &sysfont);
+		contagem_reducao_SW1 = 2 * frequencia / 3;
+		if (contagem_reducao_SW1 == 0) {
+			contagem_reducao_SW1 = 1;
+		}
 		
-		pio_set(PIOC, LED_IDX_MASK);
-		delay_ms(meio_periodo_ms);
-		pio_clear(PIOC, LED_IDX_MASK);
-		delay_ms(meio_periodo_ms);
+		if (SW1) {
+			contagem_SW1++;
+			if (contagem_SW1 >= contagem_reducao_SW1 && frequencia >= frequencia_minima + variacao_por_clique && !reduziu) {
+				frequencia -= variacao_por_clique;
+				reduziu = 1;
+			}
+		} else {
+			reduziu = 0;
+			contagem_SW1 = 0;
+		}
 		
+		if (piscando) {
+			update_oled();
+			meio_periodo_ms = 1000 / (2 * frequencia);
+			pio_set(PIOC, LED_IDX_MASK | BUZZ_IDX_MASK);
+			delay_ms(meio_periodo_ms / 2);
+			update_oled();
+			delay_ms(meio_periodo_ms / 2);
+			update_oled();
+			pio_clear(PIOC, LED_IDX_MASK | BUZZ_IDX_MASK);
+			delay_ms(meio_periodo_ms / 2);
+			update_oled();
+			delay_ms(meio_periodo_ms / 2);
+		} else {
+			pio_set(PIOC, LED_IDX_MASK | BUZZ_IDX_MASK);
+		}
 	}
 }
