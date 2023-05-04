c20d959a4cca1dfdefb9155e3c83631a73b236eb
jeronimo-a <jeronimoaa@al.insper.edu.br>
2023-05-03 22:10:07 -0300

    Tarefa 1 lab 7 feita

--- 7-RTOS-LCD-LVGL/src/main.c
+++ 7-RTOS-LCD-LVGL/src/main.c
@@ -53,6 +53,7 @@ extern void vApplicationMallocFailedHook(void) {
 /* lvgl                                                                 */
 /************************************************************************/
 
+
 static void event_handler(lv_event_t * e) {
 	lv_event_code_t code = lv_event_get_code(e);
 
@@ -62,6 +63,20 @@ static void event_handler(lv_event_t * e) {
 	else if(code == LV_EVENT_VALUE_CHANGED) {
 		LV_LOG_USER("Toggled");
 	}
+}
+
+void lv_termostato(void) {
+
+	lv_obj_t * labelBtn1;
+
+	lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+
+	labelBtn1 = lv_label_create(btn1);
+	lv_label_set_text(labelBtn1, "teste");
+	lv_obj_center(labelBtn1);
+
 }
 
 void lv_ex_btn_1(void) {
@@ -93,7 +108,7 @@ void lv_ex_btn_1(void) {
 static void task_lcd(void *pvParameters) {
 	int px, py;
 
-	lv_ex_btn_1();
+	lv_termostato();
 
 	for (;;)  {
 		lv_tick_inc(50);
